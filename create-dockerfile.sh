29f459a15597fc389c7745006e841f76dcf435d9
Niklas Grießer <niklas@griesser.me>
2024-02-06 22:50:52 +0100

    Add missing entrypoint script

--- create-dockerfile.sh
+++ create-dockerfile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/mattermost/mattermost/master/server/build/Dockerfile
+wget https://raw.githubusercontent.com/mattermost/mattermost/master/server/build/entrypoint.sh
 
 sed -i "s|ARG MM_PACKAGE=.*|ARG MM_PACKAGE=\"https://releases.mattermost.com/${VERSION}/mattermost-${VERSION}-linux-arm64.tar.gz?src=docker\"|" Dockerfile
