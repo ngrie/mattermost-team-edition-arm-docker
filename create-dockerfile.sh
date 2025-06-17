eaf6040a0cf7d86e52d744eae9d9c1e8ead39194
Niklas Grießer <niklas@griesser.me>
2025-06-17 12:20:44 +0200

    Update create-dockerfile.sh

--- create-dockerfile.sh
+++ create-dockerfile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/mattermost/mattermost/master/server/build/Dockerfile
-wget https://raw.githubusercontent.com/mattermost/mattermost/master/server/build/entrypoint.sh
+wget https://raw.githubusercontent.com/mattermost/mattermost/master/server/build/passwd
 
 sed -i "s|FROM ubuntu:.*|FROM ubuntu:noble AS builder|" Dockerfile
 sed -i "s|ARG MM_PACKAGE=.*|ARG MM_PACKAGE=\"https://releases.mattermost.com/${VERSION}/mattermost-${VERSION}-linux-arm64.tar.gz?src=docker\"|" Dockerfile
